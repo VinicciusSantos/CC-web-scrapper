d951b792ea594c4cdc6f1e51fcebfffe13eb66e1
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2023-04-13 21:37:05 -0300

    :sparkles: feat: getting all courses

---
+++ src/pages.ts
@@ -0,0 +1,7 @@
+export const BASE_URL = "https://ava.ead.unip.br/webapps"
+
+export enum Pages {
+  LOGIN = `/login`,
+  HOME = `/portal/execute/tabs/tabAction?tab_tab_group_id=_10_1`,
+  ALL_COURSES = `/portal/execute/tabs/tabAction?tab_tab_group_id=_25_1`,
+}
