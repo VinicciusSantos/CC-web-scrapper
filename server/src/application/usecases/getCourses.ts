1bd7281f8f8d3077180e9836c90f739d7cd62bbc
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2023-05-08 11:24:14 -0300

    :sparkles: feat: creating axios interceptor to check if user is authenticated

--- server/src/application/usecases/getCourses.ts
+++ server/src/application/usecases/getCourses.ts
@@ -11,17 +11,11 @@ export default class GetCoursesUsecase {
   public async execute(): Promise<Course[]> {
     const allCoursesPage = await AxiosInstance.get(Pages.ALL_COURSES);
     const $ = cheerio.load(allCoursesPage.data);
-    if (this.isInLoginPage($)) throw new NotAuthorizedError();
     const courses = $("#column0 .courseListing > li > a");
     return Object.values(courses)
       .filter((e: any) => e.attribs)
       .map((e: any) => {
         return new Course(e.children[0].data, e.attribs.href);
       });
-  }
-
-  private isInLoginPage($: cheerio.Root): boolean {
-    const res = $("#loginFormFields");
-    return res.length > 0;
   }
 }
--- server/src/infra/http/axios.ts
+++ server/src/infra/http/axios.ts
@@ -1,7 +1,20 @@
 import axios from "axios";
 import { UNIP_BASE_URL } from "../../../../entities/pages";
+import cheerio from "cheerio";
+import NotAuthorizedError from "../../application/errors/notAuthorized";
 
 export const AxiosInstance = axios.create({
   baseURL: UNIP_BASE_URL,
   withCredentials: true,
 });
+
+AxiosInstance.interceptors.response.use((response) => {
+  const $ = cheerio.load(response.data);
+  if (isInLoginPage($)) throw new NotAuthorizedError();
+  return response;
+});
+
+function isInLoginPage($: cheerio.Root): boolean {
+  const res = $("#loginFormFields");
+  return res.length > 0;
+}
